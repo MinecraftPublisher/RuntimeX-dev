459dda1c15d59550025d39a7fd61a4539986d42f
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2021-07-29 13:01:41 +0430

    dev update #3

--- src/runtimex/runtimex.compiler.js
+++ src/runtimex/runtimex.compiler.js
@@ -1,6 +1,6 @@
 /*
  *
- * Runtimex.JS
+ * Runtimex.Compiler.JS
  * Embed power in your programs.
  * Made by martia_f
  * Licensed under the MIT license.
@@ -8,16 +8,17 @@
  */
 
 const RuntimeCompiler = {};
+
 // Declare all tools and variables we need for later use.
 RuntimeCompiler.threads = {};
 RuntimeCompiler.tools = {};
 RuntimeCompiler.tools.checkConditions = function (line) {
   return (
-    line.startsWith("IF") ||
-    line.startsWith("CLASS") ||
-    line.startsWith("WHILE") ||
-    line.startsWith("FOR") ||
-    line.startsWith("FOREACH") ||
+    line.startsWith("IF ") ||
+    line.startsWith("CLASS ") ||
+    line.startsWith("WHILE ") ||
+    line.startsWith("FOR ") ||
+    line.startsWith("FOREACH ") ||
     line.startsWith(":")
   );
 };
@@ -42,14 +43,15 @@ RuntimeCompiler.compile = function (obj, nested) {
     // Declare a random number to not to break nests. ThreadID makes sure that no variables and shit gets messed up in the middle of the job.
     var ID = Math.floor(Math.random() * 10000);
     RuntimeCompiler.threads[ID] = {};
+    // Split the input into an array of lines.
+    RuntimeCompiler.threads[ID].input = obj.split("\n");
+
     // Declare the result data and craft a main thread for the current ThreadID to store the tokens inside.
     RuntimeCompiler.threads[ID].result = {
       name: "main",
-      description: "The main thread for RuntimeTokens",
+      description: nested ? "A nested main thread." : "The main thread for RuntimeTokens",
       children: []
     };
-    // Split the input into an array of lines.
-    RuntimeCompiler.threads[ID].input = obj.split("\n");
 
     // Check if the compiler is ran as nested, and if it is, return a thread.
     if (
@@ -58,18 +60,41 @@ RuntimeCompiler.compile = function (obj, nested) {
       ) &&
       nested
     ) {
-      var toReturn = RuntimeCompiler.craftThread("nested-thread", {});
+      var toReturn = RuntimeCompiler.craftThread("nested-thread", {})();
+      var line = RuntimeCompiler.threads[ID].input[0];
+
+      // Run nested components here
+      switch (line) {
+        case line.startsWith("IF "):
+          break;
+        case line.startsWith("CLASS") || line.startsWith("FOR") || line.startsWith("FOREACH") || line.startsWith("WHILE"):
+            break;
+        default:
+          console.log(
+            "Error: incorrect nested case detected, did you run element as nested?"
+          );
+          break;
+      }
     } else {
       // Loop over the lines
-      RuntimeCompiler.threads[ID].input.forEach((line, index, array) => {
+      RuntimeCompiler.threads[ID].input.forEach((line, index) => {
         // Ignore line if it's a comment.
         if (line.startsWith("#")) {
           return;
         }
         // Check if the line contains any loops/conditions/etc and return them in a recursive function. We also need to check if it's the first line, because we may create an infinite loop.
         if (index > 0 && RuntimeCompiler.tools.checkConditions(line)) {
+          var toNested = [];
+
+          for (
+            var i = index;
+            i < RuntimeCompiler.threads[ID].input.length;
+            i++
+          ) {
+            toNested.push(RuntimeCompiler.threads[ID].input[i]);
+          }
           RuntimeCompiler.threads[ID].result.children.push(
-            RuntimeCompiler.compile(obj, true)
+            RuntimeCompiler.compile(toNested.join("\n"), true)
           );
         }
       });
